cb46bf793c15d7737fc8652710ca5fea95b7e08f
Ulyana Procopjuk <ulianaprocop@mail.ru>
2024-10-09 22:53:35 +0300

    Created project and added .gitignore

---
+++ Weather/SceneDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        // Создаем окно
+        window = UIWindow(windowScene: windowScene)
+
+        let initialViewController = ViewController() // Ваш корневой контроллер
+        window?.rootViewController = initialViewController
+
+        window?.makeKeyAndVisible()
+    }
+}
+
