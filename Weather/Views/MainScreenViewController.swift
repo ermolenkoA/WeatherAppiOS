44157425d207a2ec2d4372fb1b080a399d26b2c3
Ulyana Procopjuk <ulianaprocop@mail.ru>
2024-10-16 22:41:16 +0300

    Added RSwift

--- Weather/Views/MainScreenViewController.swift
+++ Weather/Views/MainScreenViewController.swift
@@ -1,12 +1,14 @@
 import UIKit
+import Rswift
 
 final class MainScreenViewController: UIViewController {
 
     var presenter: MainScreenPresenter?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
+
     }
 
 }
